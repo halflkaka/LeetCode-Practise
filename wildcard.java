22a83fd07e97384c868ca5539248cdf08767bed9
halflkaka <cs3736@columbia.edu>
2018-12-23 23:55:15 -0500

    12/23/2018

---
+++ wildcard.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*') {
+                dp[0][i+1] = true;
+            } else {
+                break;
+            }
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '?' || s.charAt(i) == p.charAt(j)) {
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*') {
+                    dp[i+1][j+1] = dp[i][j] || dp[i+1][j] || dp[i][j+1];
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+}
