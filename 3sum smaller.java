45a0312f6cb9a7aa3cf70aa82c465b976ee154ad
halflkaka <cs3736@columbia.edu>
2018-12-26 17:47:26 -0500

    Merry Christmas

---
+++ 3sum smaller.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        int count = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i+1, right = nums.length - 1;
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] < target) {
+                    count += right - left;
+                    left ++;
+                } else {
+                    right --;
+                }
+            }
+        }
+        return count;
+    }
+}
+//-4, -1, -1, 0, 1, 2
